2ed7433f16438cdd6b213aefffd4f3ba9b515053
Damyan Boychev <dboychev@astea.net>
2019-12-20 11:54:59 +0200

    Add solutions; error in cpy, must have null char at the end

---
+++ Lesson12/solutions.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <cstring> // cin, cout for char[]
+using std::cin;
+using std::cout;
+using std::endl;
+
+void printASCIITable() {
+  for(unsigned char c = 0; ; c++) {
+    cout << "Character #" << (int)c << ": \"" << c << "\"" << endl;
+    if(c == 255) {
+      break;
+    }
+  }
+}
+
+int strlen(char str[]) {
+  int length = 0;
+  for(int i = 0; ; i++) {
+    if(str[i] != '\0') {
+      length++;
+    } else {
+      break;
+    }
+  }
+
+  return length;
+}
+
+int strcmp(char lhs[], char rhs[]) {
+  int lhsLength = strlen(lhs);
+  int rhsLength = strlen(rhs);
+
+  for(int i = 0; i < lhsLength && i < rhsLength; i++) {
+    // if(lhs[i] < rhs[i]) {
+    //   return -1;
+    // } else if(lhs[i] > rhs[i]) {
+    //   return 1;
+    // }
+
+    if(lhs[i] != rhs[i]) {
+      return lhs[i] - rhs[i];
+    }
+  }
+
+  // if(lhsLength < rhsLength) {
+  //   return -1;
+  // } else {
+  //   return 1;
+  // }
+
+  return lhsLength - rhsLength;
+}
+
+void strcpy(char destination[], char source[]) {
+  int sourceLength = strlen(source);
+
+  for(int i = 0; i < sourceLength; i++) {
+    destination[i] = source[i];
+  }
+}
+
+
+void strcat(char destination[], char source[]) {
+  int destinationLength = strlen(destination);
+
+  // Copy the source to the end of the destination
+  strcpy(destination + destinationLength, source);
+}
+
+int main() {
+  char str1[] = "ASD";
+  char str2[] = "HJK";
+
+  strcat(str1, str2);
+
+  cout << str1 << endl;
+}
