30decff5ec3f5b100f0dddcf705c89881203d8fa
Damyan Boychev <dboychev@astea.net>
2020-01-11 18:40:09 +0200

    Add code, written on consultation

--- Dopulnitelno/solutions.cpp
+++ Dopulnitelno/solutions.cpp
@@ -81,13 +81,17 @@ void countSym(char str[], int wordCount[]){
 
 }
 
+bool areEqual(int wordCount1[], int wordCount2[]){
+    for(int i = 0; i < 26; i++){
+      if(wordCount1[i] != wordCount2[i]){
+          return false;
+      }
+    }
 
+    return true;
+}
 
-
-
-
-
-int main() {
+void secondTask(){
   int wordCountStr1[26] = {0}, wordCountStr2[26] = {0};
   char str1[128], str2[128];
 
@@ -95,11 +99,73 @@ int main() {
   cin.getline(str2,128);
 
   countSym(str1,wordCountStr1);
+  countSym(str2,wordCountStr2);
 
-  for(int i = 0; i < 26; i++){
-    cout<<wordCountStr1[i]<<" ";
-  }
   cout<<endl;
+
+  cout<<areEqual(wordCountStr1, wordCountStr2)<<endl;
+}
+
+// ---------------------------------------------------------------
+
+
+double getMult(double arr[],int size) {
+  if(size == 1) {
+    return 1;
+  }
+  if(size == 2) {
+    if(arr[0]>arr[1] && arr[1] > 0) {
+        return arr[1];
+    }
+    return 1;
+  }
+
+
+  if(arr[0]> arr[1] && arr[1] > 0){
+    return arr[1] * getMult(arr+1,size-1);
+  }
+  return getMult(arr+1,size-1);
+}
+
+
+void testGetMult() {
+  double arr[100];
+  int n;
+  cin>>n;
+  for(int i = 0;i<n;i++ ){
+    cin>> arr[i];
+  }
+  cout<<getMult(arr,n)<<endl;
+}
+// ------------------------------------------------------------
+
+int longestColIndex(int matrix[][5],int m,int arr[], int n){
+  int biggestIndex = 0;
+  int biggestMatchingElements =0;
+  for(int i=0;i<5 ;i++)
+  {
+      int matchingElements =0;
+      for(int j=0;j<m && j<n ;j++)
+      {
+        if(matrix[j][i] == arr[j])
+        {
+          matchingElements++;
+        }
+      }
+      if(matchingElements>biggestMatchingElements)
+      {
+        biggestIndex = i;
+        biggestMatchingElements = matchingElements;
+
+      }
+  }
+
+  return biggestIndex;
+}
+
+void testLongestColIndex() {}
+
+int main() {
 
   return 0;
 }
